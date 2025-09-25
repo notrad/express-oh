0747b370013682096b627a355bdfc22047fb7ee0
notrad <anoopbevendcunha@gmail.com>
2025-09-25 22:13:17 +0530

    Updated auth middleware logic(usage not update)

--- src/middlewares/authHandler.ts
+++ src/middlewares/authHandler.ts
@@ -1,25 +1,22 @@
 import type { Response, NextFunction } from "express";
-import jwt from "jsonwebtoken";
-import type { AuthenticatedRequest, UserRole, JwtPayload } from "../types/Auth";
+import { generateToken, login, verifyToken } from "../services/authService";
+import type { AuthenticatedRequest, UserRole } from "../types/Auth";
 
-const JWT_SECRET = process.env.JWT_SECRET || "secret";
-
-export const authenticate = (
+export async function authenticate(
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction,
-) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({
-      status: "error",
-      message: "No token provided",
-    });
-  }
-
-  const token = authHeader.split(" ")[1];
+) {
   try {
-    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    const token = extractTokenFromHeader(req);
+    if (!token) {
+      return res.status(401).json({
+        status: "error",
+        message: "No token provided",
+      });
+    }
+
+    const decoded = await verifyToken(token);
     req.user = decoded;
     next();
   } catch (error) {
@@ -28,9 +25,9 @@ export const authenticate = (
       message: "Invalid token",
     });
   }
-};
+}
 
-export const authorize = (...roles: UserRole[]) => {
+export async function authorize(...roles: UserRole[]) {
   return (req: AuthenticatedRequest, res: Response, next: NextFunction) => {
     if (!req.user) {
       return res.status(401).json({
@@ -50,4 +47,13 @@ export const authorize = (...roles: UserRole[]) => {
 
     next();
   };
-};
+}
+
+function extractTokenFromHeader(req: AuthenticatedRequest): string | null {
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return null;
+  }
+
+  return authHeader.split(" ")[1];
+}
