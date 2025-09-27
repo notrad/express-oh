2925f170daac29eb45deb3619eb65a1699b3cd70
notrad <anoopbevendcunha@gmail.com>
2025-09-27 21:58:21 +0530

    Code refactoring

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,11 +2,11 @@ import type { Request, Response } from "express";
 import type { CreateUserDto, UserResponse } from "../types/User";
 import type { ApiResponse } from "../types/Api";
 
-export function getUser(req: Request, res: Response) {
+export const getUser = (req: Request, res: Response) => {
   res.json({ message: "Received GET" });
-}
+};
 
-export function postUser(req: Request, res: Response) {
+export const postUser = (req: Request, res: Response) => {
   const newUser: CreateUserDto = req.body;
   const response: ApiResponse<UserResponse> = {
     status: "success",
@@ -17,29 +17,29 @@ export function postUser(req: Request, res: Response) {
   };
 
   res.json(response);
-}
+};
 
-export function putUser(req: Request, res: Response) {
+export const putUser = (req: Request, res: Response) => {
   const userId: string = req.params.id;
   const updatedFields = req.body;
   res.json({
     message: `Received PATCH for ${userId}`,
     data: updatedFields,
   });
-}
+};
 
-export function patchUser(req: Request, res: Response) {
+export const patchUser = (req: Request, res: Response) => {
   const userId: string = req.params.id;
   const updatedFields = req.body;
   res.json({
     message: `Received a PATCH HTTP method for user ${userId}`,
     data: updatedFields,
   });
-}
+};
 
-export function deleteUser(req: Request, res: Response) {
+export const deleteUser = (req: Request, res: Response) => {
   const userId: string = req.params.id;
   res.json({
     message: `Received a DELETE for ${userId}`,
   });
-}
+};
