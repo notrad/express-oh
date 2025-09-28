97537d2a6e0e251234ecfed79398625bcd3a84c4
notrad <anoopbevendcunha@gmail.com>
2025-09-28 20:29:18 +0530

    Added middleware to handle validation

---
+++ src/middlewares/validationHandler.ts
@@ -0,0 +1,21 @@
+import type { Request, Response, NextFunction } from "express";
+import type { Schema } from "joi";
+
+export const validate = (schema: Schema) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { error } = schema.validate(req.body, {
+      abortEarly: false,
+      stripUnknown: true,
+    });
+
+    if (error) {
+      return res.status(400).json({
+        status: "error",
+        message: "Validation failed",
+        errors: error.details.map((err) => err.message),
+      });
+    }
+
+    next();
+  };
+};
