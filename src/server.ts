f96435409ed89b307ee1ba431fee9d49c7a17c66
notrad <anoopbevendcunha@gmail.com>
2025-10-09 21:40:11 +0530

    Added DB initialization to application startup flow

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import createApp from "./app";
 import { appConfig, validateConfig } from "./config/config";
+import { getDatabase, initializeDatabase } from "./common/utils/bootstrap";
 
 const startServer = async () => {
   try {
     validateConfig();
+
+    await initializeDatabase();
+
     const app = createApp();
 
     app.listen(appConfig.port, () => {
@@ -13,6 +17,15 @@ const startServer = async () => {
         `Documentations available at: http://localhost:${appConfig.port}/api-docs`,
       );
     });
+
+    const gracefulShutdown = async () => {
+      const db = getDatabase();
+      await db.disconnect();
+      process.exit(0);
+    };
+
+    process.on("SIGTERM", gracefulShutdown);
+    process.on("SIGINT", gracefulShutdown);
   } catch (error) {
     console.error("Failed to start server:", error);
     process.exit(1);
