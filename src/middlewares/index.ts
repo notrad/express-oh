5e422391760887a955bdf2cda253020726be47e2
notrad <anoopbevendcunha@gmail.com>
2025-10-23 22:06:12 +0530

    Updated middleware enntry file

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -4,3 +4,15 @@ import morgan from "morgan";
 import cors from "cors";
 import compression from "compression";
 import { errorHandler } from "./errorHandler";
+import { notFoundHandler } from "./notFoundHandler";
+import { devCorsOptions } from "../common/constants/corsOptions";
+
+export const registerMiddlewares = (app: Express) => {
+  app.use(compression());
+  app.use(morgan("dev"));
+  app.use(cors(devCorsOptions));
+  app.use(express.json());
+
+  app.use("*", notFoundHandler);
+  app.use(errorHandler);
+};
