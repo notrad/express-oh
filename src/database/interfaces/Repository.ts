cbcd61e313fe8af2a6039f5b05567b635badff6f
notrad <anoopbevendcunha@gmail.com>
2025-11-05 16:59:05 +0530

    Code clean up

--- src/database/interfaces/Repository.ts
+++
@@ -1,7 +0,0 @@
-export interface Repository<T> {
-  findOne(id: string): Promise<T | null>;
-  findAll(): Promise<T[]>;
-  create(data: Partial<T>): Promise<T>;
-  update(id: string, data: Partial<T>): Promise<T>;
-  delete(id: string): Promise<void>;
-}
