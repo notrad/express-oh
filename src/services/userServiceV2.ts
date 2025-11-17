8f0082d1a7fcb9f77a718cf01f7830e1b31a8046
notrad <anoopbevendcunha@gmail.com>
2025-11-17 22:09:10 +0530

    Added user service impl

--- src/services/userServiceV2.ts
+++ src/services/userServiceV2.ts
@@ -12,3 +12,19 @@ export const createUser = async (user: CreateUserDto) => {
   const result = await db.collection("users").insertOne(user);
   return { ...user, _id: result.insertedId };
 };
+
+export const updateUser = async (id: string, user: UpdateUserDto) => {
+  const db = await getDb();
+  const result = await db
+    .collection("users")
+    .updateOne({ _id: new ObjectId(id) }, { $set: user });
+  return { ...user, _id: result.upsertedId };
+};
+
+export const deleteUser = async (id: string) => {
+  const db = await getDb();
+  const results = await db
+    .collection("users")
+    .deleteOne({ _id: new ObjectId(id) });
+  return results.acknowledged;
+};
