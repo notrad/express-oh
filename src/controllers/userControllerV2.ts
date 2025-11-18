019bc31054c2077bef38ee28fbfa8e6cb948767e
notrad <anoopbevendcunha@gmail.com>
2025-11-18 21:38:14 +0530

    Added user controller V2

--- src/controllers/userControllerV2.ts
+++ src/controllers/userControllerV2.ts
@@ -1,9 +1,37 @@
 import type { Request, Response } from "express";
-import { findUserById } from "../services/userServiceV2";
-import type { CreateUserDto, UpdateUserDto } from "../types/User";
+import { createUser, findUserById } from "../services/userServiceV2";
+import type { CreateUserDto, UpdateUserDto, UserResponse } from "../types/User";
+import type { ApiResponse } from "../types/Api";
 
 export const getUserV2 = async (req: Request, res: Response) => {
-  const user = await findUserById(req.params.id);
-  if (!user) return res.status(404).json({ messsage: "User not found" });
-  res.json(user);
+  try {
+    const user = await findUserById(req.params.id);
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+    }
+    const response: ApiResponse<UserResponse> = {
+      status: "success",
+      data: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    res.json(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+};
+
+export const postUserV2 = async (req: Request, res: Response) => {
+  const user: CreateUserDto = req.body;
+  const createdUser = await createUser(user);
 };
